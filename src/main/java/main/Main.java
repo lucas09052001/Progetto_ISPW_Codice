51c867eb2529a1ad7fe9f9c57410a65ad1d21b33
Lucas <lucas09052001@gmail.com>
2025-07-03 06:06:28 +0200

    Test per Branches Intellij

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,9 @@ import boundery.BounderyEnum;
 import entity.SessionInfo;
 
 public class Main {
+
+    //Il commento pazzo sgravato
+
     public static void main(String[] args) {
         SessionInfo sessionInfo = SessionInfo.getSessionInfo();
         sessionInfo.setNextBoundery(BounderyEnum.AUTHENTICATE);
