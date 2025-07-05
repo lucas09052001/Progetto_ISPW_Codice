8315a2dddbcb0097fece65a57c66fa4838f1c3db
Lucas <lucas09052001@gmail.com>
2025-07-05 11:44:00 +0200

    SonarCloudCleansing-HIGH. Tentativo:2

--- src/main/java/utilities/PathUtility.java
+++ src/main/java/utilities/PathUtility.java
@@ -2,17 +2,20 @@ package utilities;
 
 public class PathUtility {
 
-    private static final String PATH_TO_EMPTY_IMAGE = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/resources/ImageRepository/EmptyImage.png";
-    private static final String PATH_TO_LOAN_REQUEST_JSON = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/resources/Json/loanRequests.json";
-    private static final String PATH_TO_LOAN_POST_JSON = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/resources/Json/loanPost.json";
-    private static final String PATH_TO_LOAN_EFFECTIVE_JSON = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/resources/Json/loanEffective.json";
-    private static final String PATH_TO_DISCOUNT_JSON = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/resources/Json/discount.json";
-    private static final String PATH_TO_USER_JSON = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/resources/Json/users.json";
-    private static final String PATH_TO_LOGO_IMAGE = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/resources/ImageRepository/Logo.png";
-    private static final String PATH_TO_ZERO_STAR = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/src/main/java/boundery/profile_boundery/NoRating.png";
-    private static final String PATH_TO_ONE_STAR = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/src/main/java/boundery/profile_boundery/1star.png";
-    private static final String PATH_TO_TWO_STAR = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/src/main/java/boundery/profile_boundery/2star.png";
-    private static final String PATH_TO_THREE_STAR = "/home/lucas/Documents/Università/Corrente/ISPW/Progetto_Codice/progetto_ISPW/src/main/java/boundery/profile_boundery/3star.png";
+    private static final String PATH_TO_EMPTY_IMAGE = "resources/ImageRepository/EmptyImage.png";
+    private static final String PATH_TO_LOAN_REQUEST_JSON = "resources/Json/loanRequests.json";
+    private static final String PATH_TO_LOAN_POST_JSON = "resources/Json/loanPost.json";
+    private static final String PATH_TO_LOAN_EFFECTIVE_JSON = "resources/Json/loanEffective.json";
+    private static final String PATH_TO_DISCOUNT_JSON = "resources/Json/discount.json";
+    private static final String PATH_TO_USER_JSON = "resources/Json/users.json";
+    private static final String PATH_TO_LOGO_IMAGE = "resources/ImageRepository/Logo.png";
+    private static final String PATH_TO_ZERO_STAR = "resources/ImageRepository/NoRating.png";
+    private static final String PATH_TO_ONE_STAR = "resources/ImageRepository/1star.png";
+    private static final String PATH_TO_TWO_STAR = "resources/ImageRepository/2star.png";
+    private static final String PATH_TO_THREE_STAR = "resources/ImageRepository/3star.png";
+
+
+
 
     private PathUtility() {
         throw new IllegalStateException("Utility class");
